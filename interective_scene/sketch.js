26809bc5bf22a9cc4134fb0678b18f9312093e2c
7015997-hash <7015997@spsd.sk.ca>
2026-03-03 15:25:45 -0600

    changed the whole thing

--- interective_scene/sketch.js
+++ interective_scene/sketch.js
@@ -1,83 +1,76 @@
-// Project Title
-// Your Name
-// Date
+// PingPong Ball
+// Mobashira Hossain Naba
+// March 3rd 2026
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x = 200;
+let y = 200;
+let dx = 4;
+let dy = 3;
+let batW = 100;
+let batH = 10;
+let batY = 350;
+let counter = 0;
+let bgMusic;
 
-let img;
-let imgShooter;
-let bullets = [];
-let enemies = [];
-
-function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < 10; i++) {
-    let enemy = {
-      x: random(0, width),
-      y: random(-800, 0),
-    };
-    enemies.push(enemy);
-  }
-  
-}
-function preload() {
-  img = loadImage("Pixel art bg.jpg");
-  imgShooter = loadImage("charac.png");
-}
-
-function draw() {
-  image(img, 0, 0, width, height);
-  // background(240, 182, 193);
+function setup(){
+  createCanvas(400,400);
   rectMode(CENTER);
-  drawShooter();
-  bullet();
-  enemy();
+  for (let counter = 1; counter <= 10; counter++) {
+    console.log(counter);
+  }
   
 }
+// Background Image changing.
+function preload() {
+ img = loadImage("Pixel art bg.jpg");
+ bgMusic = loadSound("bgMusic.mp3");
 
-//   shooter
-// function preload() {
-//  
- 
+}
 
-// }
-function drawShooter() {
-  fill("red");
+
+function draw(){
+  background(200);
+  text("Score:",15,40)
+  textSize(15);
+  bat();
+  drawBall();
+}
+// The paddle
+function bat(){
+
+  fill("black");
+  rect(mouseX,batY,batW,batH);
+}
+// Ball bouncing
+function drawBall(){
+  fill(200,25,26);
+  ellipse(x,y,40);
   noStroke();
-  // rect(mouseX, height - 50, 20, 20);
-  image(imgShooter ,mouseX, height - 50, 20, 20);
-}
-function enemy() {
-  for (let enemy of enemies) {
-    fill("black");
-    enemy.y += 2;
-    rect(enemyX, enemy, 10);
+  x += dx;
+  y += dy;
+  if (x > width || x < 0){
+    dx =  dx *-1;
   }
-}
+  if (y > height || y < 0){
+    dy =  dy *-1;
+  }
+  // Detetion collision...
+  if ((x >= mouseX && x <= mouseX+ batW)&& (y + 12.5 >= batY)){
+    x *= -1;
+    y *= -1;
+    x *= -1;
+    x *= -1;
+    counter += 1;
 
-for (let enemy of enemies) {
-  for (let bullet of bullets) {
-    if (dist(enemy, enemy, bullet.x, bullet.y) < 10) {
-    
-      
-    }
   }
+  // when clicked bg changes.
+  function keyPressed(){
+  if (key === "a"){
+  image(img, 0, 0, width, height);
+ }
 }
-function bullet() {
-  stroke("black");
-  fill("white");
-  for (let bullet of bullets) {
-    circle(bullet.x, bullet.y, 10);
-    bullet.y -= 10;
-  }
-}
+} 
 
-function mousePressed() {
-  let bullet = {
-    x: mouseX,
-    y: height - 50,
-  };
-  bullets.push(bullet);
-}
