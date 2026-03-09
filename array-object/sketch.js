bb1b4cc0fc7479a42d8a71432963c827dbbf711a
7015997-hash <7015997@spsd.sk.ca>
2026-03-09 15:25:21 -0600

    try to find 3d model

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -1,13 +1,13 @@
 // Arrays and Object notation
 // Mobashira Hossain
-// march 5/26
+// March 5/26
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400,400,WEBGL);
 }
 
 function draw() {
