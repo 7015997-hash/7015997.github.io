182e51d5d224b4cf21959ed5ac325a15c3712d55
7015997-hash <7015997@spsd.sk.ca>
2026-03-11 16:47:28 -0600

    grid ready

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -6,21 +6,34 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let rows=10 ;
-let colums= 8;
+let rows=40 ;
+let colums=40;
 let colSize;
 let rowSize;
+let marginRight = 100;
 
 
 
 
 
 function setup() {
-  createCanvas(400,400,WEBGL);
+  createCanvas(windowWidth,windowHeight);
+
   colSize = width/colums;
   rowSize = height/rows;
 }
 
 function draw() {
   background(220);
+  for (let i=0; i < colums; i++){
+    for (let j = 0; j<rows; j++){
+      rect(i*colSize, j*rowSize,colSize,rowSize);
+    }
+  
+  }
+}
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+
+
 }
