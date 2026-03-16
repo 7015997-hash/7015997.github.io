18205ac32a08acdc8ab5e556a100a62d59c2fa8e
7015997-hash <7015997@spsd.sk.ca>
2026-03-16 15:24:59 -0600

    Created some more blocks

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -3,7 +3,7 @@
 // March 5/26
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - used buffer canvas which helped me to make two layer (with help) ,used the bezierVertex from reference and made a s-curve shape.Used a texture for the middle circle.
 
 
 
@@ -15,14 +15,14 @@ function setup() {
 
 function draw() {
   background(204,204,0);
-//   Buffer canvas create
+  //   Buffer canvas create
   cnv1 = createGraphics(width,height);
   cnv1.background(255,255,102);
   cnv1.fill(255,204,229);
   cnv1.circle(250,200,150);
   img = cnv1.get(width-350,height-400,410,350);
   image(img,0,0);
-//   all the shapes:
+  // all the shapes:
   a1();
   b2();
   c3();
@@ -39,16 +39,25 @@ function a1(){
   
   fill(0,153,153);
   square(330,200,50);
-  
-//   fill(255,102,102);
-//   rect(60,70,390,10);
-  
-//   fill(255,204,204);
-//   rect(70,58,390,10);
-  
-//   fill(204,255,255);
-//   rect(5,46,390,10);
-  
+
+  fill(51,155,253);
+  square(50,50,50);
+
+  fill(102,0,51);
+  square(150,50,50);
+
+  fill(204,0,0);
+  square(100,0,50);
+
+  fill(204,102,0);
+  square(200,0,50);
+
+  fill(102,102,0);
+  square(250,50,50);
+
+  fill(204,204,255);
+  square(300,0,50);
+
 }
 function b2(){
   fill(255,204,229,150);
@@ -68,6 +77,8 @@ function d4(){
   rect(100,100,200,100);
   noStroke();
 }
+
+//  All the arcs
 function e5(){
   fill(255,204,204);
   angleMode(DEGREES);
@@ -82,6 +93,7 @@ function e5(){
   arc(350,399,90,80,270,-360); 
   
 }
+// The textured circle
 function cross(){
   push();
   translate(height/2,width/2);
@@ -96,6 +108,7 @@ function cross(){
   }
   pop();
 }
+// The bottom curve
 function s(){
   fill(255,255,102);
   stroke(255,153,204);
@@ -108,8 +121,9 @@ function s(){
   endShape(CLOSE);
   
 }
+//  The right curve
 function s2(){
-fill(255,255,102);
+  fill(255,255,102);
   stroke(255,153,204);
   beginShape();
   vertex(width, 0);       
@@ -121,6 +135,9 @@ fill(255,255,102);
   vertex(width, height);  // Bottom-right corner
   endShape(CLOSE);
 }
+
+
+
 
 
 
