16c0f18c87542bc780c358be8c804a5d14a2e635
7015997-hash <7015997@spsd.sk.ca>
2026-03-19 13:05:21 -0600

    done

--- 7-grid/sketch.js
+++ 7-grid/sketch.js
@@ -8,21 +8,35 @@
 // 2D Grid Demo
 //learning 2d arrays
 
-let theGrid = [[1, 0, 1, 0],
-               [1, 0, 1, 0],
-               [0, 1, 0, 1],
-               [0, 0, 1, 1]];
-const SQUARE_DIMENSION = theGrid.length;
+// let theGrid = [[1, 0, 1, 0],
+//                [1, 0, 1, 0],
+//                [0, 1, 0, 1],
+//                [0, 0, 1, 1]];
+// const SQUARE_DIMENSION = theGrid.length;
+// Grid Demo
+//learning 2d arrays
+
+//use this if hard coding the grid
+// let theGrid = [[0, 0, 1, 0],
+//                [1, 0, 1, 0],
+//                [0, 1, 1, 0],
+//                [0, 1, 0, 1]];
+// const SQUARE_DIMENSIONS = theGrid.length;
+
+//use this if randomizing the grid
+const SQUARE_DIMENSIONS = 15;
+let theGrid;
 let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if (width > height) {
-    cellSize = height/SQUARE_DIMENSION;
+  if (width < height) {
+    cellSize = width / SQUARE_DIMENSIONS;
   }
   else {
-    cellSize = width/SQUARE_DIMENSION;
+    cellSize = height / SQUARE_DIMENSIONS;
   }
+  theGrid = randomizeGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
 }
 
 function draw() {
@@ -30,20 +44,30 @@ function draw() {
   showGrid();
 }
 
-function mousePressed(){
+function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
+  // console.log(x, y);
+  toggleCell(x, y);
+}
 
+function toggleCell(x, y) {
+  if (theGrid[y][x] === 1) {
+    theGrid[y][x] = 0;
+  }
+  else if (theGrid[y][x] === 0) {
+    theGrid[y][x] = 1;
+  }
 }
 
 function showGrid() {
-  for (let y = 0; y < SQUARE_DIMENSION; y++) {
-    for (let x = 0; x < SQUARE_DIMENSION; x++) {
+  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
+    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
       if (theGrid[y][x] === 1) {
         fill("black");
       }
-      else if (theGrid[y][x] === 0) {
+      if (theGrid[y][x] === 0) {
         fill("white");
       }
       square(x * cellSize, y * cellSize, cellSize);
@@ -51,11 +75,18 @@ function showGrid() {
   }
 }
 
-function mousePressed(){
-  if (mouseIsPressed === 1){
-    return true;
+function randomizeGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[y].push(1);
+      }
+    }
   }
-  else{
-    return false;
-  }
+  return newGrid;
 }
