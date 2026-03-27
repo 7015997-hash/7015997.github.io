c25a9eb3af2005257b4c7228c83c476fc869b11e
7015997-hash <7015997@spsd.sk.ca>
2026-03-27 15:25:24 -0600

    tried to put the tiles

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -10,15 +10,17 @@
 let rows = 5;
 let cols = 5;
 let board=[];
+let tiles=[];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+ 
+
 }
 
 function draw() {
   background(220);
   drawgrid();
-  makeResize();
 
 }
 
@@ -26,19 +28,10 @@ function   drawgrid(){
   let colSize = width/cols;
   let rowSize = height/rows;
 
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      rect(i*colSize, j*rowSize,colSize,rowSize);
-
-
+  for (let x = 0; x < cols; x++){
+    for (let y = 0; y < rows; y++){
+      rect(x*colSize, y*rowSize,colSize,rowSize);
     }
-
-
   }
-  function makeResize(){
-    resizeCanvas(windowWidth,windowHeight);
-  }
-
-
 
 }
