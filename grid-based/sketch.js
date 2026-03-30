8839d1416bc8ac173c95e01b1e325fb01822b69f
7015997-hash <7015997@spsd.sk.ca>
2026-03-30 15:24:36 -0600

    Made menu page

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -1,4 +1,4 @@
-// Project Title
+// Grid Based
 // MObashira Naba
 // Date
 //
@@ -6,32 +6,48 @@
 // - describe what you did to take this project "above and beyond"
 
 
-
-let rows = 5;
-let cols = 5;
-let board=[];
-let tiles=[];
+let screen = 0;
+let button;
+let button2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
- 
+  button = createButton('Puzzle');
+  button.position(width/2, height/2);
+  button.mousePressed(startGame);
 
+  button2 = createButton('Tic tae Toe');
+  button2.position(width/2, height/5);
+  button2.mousePressed(startGame);
 }
 
 function draw() {
-  background(220);
-  drawgrid();
+  // background(220);
+  if (screen === 0){
+    drawMenu();
+  }
+  else if (screen ===1 ){
+    drawGame();
+  }
 
-}
+  function drawMenu(){
+    background(200);
+    textAlign(CENTER);
+    textSize(30);
+    fill(255);
+    text('MAin', width/2, height/3);
+    button.show();
+    button2.show();
+  }
+  function drawGame() {
+    button.hide();
+    button2.hide();
+    background(50, 150, 50);
+    text('Game Screen', width/2, height/2);
+  }
 
-function   drawgrid(){
-  let colSize = width/cols;
-  let rowSize = height/rows;
-
-  for (let x = 0; x < cols; x++){
-    for (let y = 0; y < rows; y++){
-      rect(x*colSize, y*rowSize,colSize,rowSize);
-    }
+  function startGame() {
+    screen = 1;
   }
 
 }
